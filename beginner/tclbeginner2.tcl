befa75bcf38b91d7aaa0c21c204f2f937b557cdb
Thahmidul Islam Nafi <nafi@PST_NAFI.localdomain>
2022-10-14 14:19:04 -0700

    2nd problem solved and uploaded

---
+++ beginner/tclbeginner2.tcl
@@ -0,0 +1,30 @@
+
+puts "Enter a number for fibonacci series: "
+set num [gets stdin]
+
+proc fib {n} {
+
+  global fibcache
+	if { $n == 1 || $n == 0 } {
+		if { [llength $fibcache] < 2 } {
+			lappend fibcache 1
+		}
+		return 1
+	} else {
+		if {[llength $fibcache] > [expr {$n + 1}]} {
+			return [lindex $fibcache $n]
+		}	else {
+			lappend fibcache [expr {[fib [expr {$n-1}]] + [fib [expr {$n-2}]] }]
+			return [lindex $fibcache $n]
+		}
+		 
+	}
+}
+
+set fibcache {}
+
+
+puts "[fib $num]"
+
+puts "$fibcache"
+
