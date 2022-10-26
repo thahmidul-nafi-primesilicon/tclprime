70fc0918a988f55a4e203fbe3bcc5e04a92d7c44
Thahmidul Islam Nafi <nafi@PST_NAFI.localdomain>
2022-10-26 09:38:54 -0700

    Added beginner problem 10

--- beginner/blockoutline.tcl
+++ beginner/blockoutline.tcl
@@ -1,10 +1,15 @@
-canvas .myCanvas -background red -width 200 -height 200 
+
+canvas .myCanvas -background red -width 900 -height 900 
+
+set poly { 0 0 655 0 655 4300 790 4300 790 4700 655 4700 655 7413 0 7413 }
+set poly2 { 0 0 835 0 835 4300 970 4300 970 4700 835 4700 835 7413 0 7413 }
+set polyscaled [lmap a $poly {expr {$a / 15 + 50}}]
+set polyscaled2 [lmap a $poly2 {expr {$a / 15 + 50}}]
+
+.myCanvas create polygon $polyscaled2 -fill white -outline black
+.myCanvas create polygon $polyscaled -fill blue -outline black
+
+# .myCanvas create polygon 200 200 250 200 250 275 200 275 -fill blue -outline black 
+
+
 pack .myCanvas
-set itemlist {}
-set block {{0.0000 0.0000} {654.9930 0.0000} {654.9930 4300.6880} {790.6530 4300.6880} {790.6530 4700.7520} {654.9930 4700.7520} {654.9930 7413.9520} {0.0000 7413.9520}}
-foreach item $block {
-	foreach term $item {
-		lappend itemlist $term
-	}
-}
-.myCanvas create polygon $itemlist -fill yellow -outline green
--- beginner/tclbeginner10.tcl
+++ beginner/tclbeginner10.tcl
@@ -5,36 +5,36 @@ set n [gets stdin]
 for {set i 0} {$i <$n} {incr i} {
 	set cachedout {}
 	for {set j 0} {$j <$n} { incr j} {
-		
+		if {$i<3 && $j<3} {
+			puts -nonewline " "
+		}
 			
 		if { $i == $j } {
 		set value [expr {[expr {$i + 1}] ** 2 -$i}]	
-			lappend cachedout $value
+			puts -nonewline "$value "
 		
 
 		}
 		if {$i > $j} {
 			if {[expr {$i%2}]==0} {
-				lappend cachedout [expr {[expr {[expr {$i+1}]**2}] - [expr {2*[expr {$i+1}]-2-$j}]}]
+				puts -nonewline "[expr {[expr {[expr {$i+1}]**2}] - [expr {2*[expr {$i+1}]-2-$j}]}] "
 			} else {
-				lappend cachedout [expr {[expr {[expr {$i+1}]**2}] - [expr {$j}]}]
+				puts -nonewline "[expr {[expr {[expr {$i+1}]**2}] - [expr {$j}]}] "
 			}
 		}
 		if {$j > $i} {
 
 			if {[expr {$j%2}]==1} {
-				lappend cachedout [expr {[expr {[expr {$j+1}]**2}] - [expr {2*[expr {$j+1}]-2-$i}]}]
+				puts -nonewline "[expr {[expr {[expr {$j+1}]**2}] - [expr {2*[expr {$j+1}]-2-$i}]}] "
 			} else {
-				lappend cachedout [expr {[expr {[expr {$j+1}]**2}] - [expr {$i}]}]
+				puts -nonewline "[expr {[expr {[expr {$j+1}]**2}] - [expr {$i}]}] "
 			}
 		}
 		
 	
 	
 }
-if {$i<3} {
-	puts -nonewline "   "
+puts ""
 }
-puts $cachedout
-}
+
 
